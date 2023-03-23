28b99fab9bc5b44817887c34153ef659d18dc052
Abdou Ayoub <abdou999>
2023-03-23 16:48:52 +0200

    ver1

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+ int x;
+ 
+ printf ("Enter The Number\n");
+ scanf ("%d",&x);
+ 
+ 
+ if (x%2==0)
+ 
+ {
+ printf ("The number is odd=0\n");
+ printf ("The number is even=1\n"); 
+ }
+ 
+ if (x%2==1)
+ {
+ printf ("The number is odd=1\n");
+ printf ("The number is even=0\n"); 
+ }
+ return 0;}
